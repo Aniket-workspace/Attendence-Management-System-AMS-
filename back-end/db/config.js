f9309a7f1fe5650b29156102e694645b01302962
Aniket Gawai <aniketgawai88@gmail.com>
2024-07-19 20:37:54 +0530

    MongoDB atlas url added

--- back-end/db/config.js
+++ back-end/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://0.0.0.0:27017/ams");
+mongoose.connect("mongodb+srv://aniketgawai88:aniket123@ams.lirqsoi.mongodb.net/ams?retryWrites=true&w=majority&appName=AMS");
